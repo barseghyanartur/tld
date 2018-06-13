63e7989edf05dce7a5c10f7f4c7b7a155e0d7b96
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-06-14 01:46:38 +0200

    prepare 0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ try:
 except:
     readme = ''
 
-version = '0.8'
+version = '0.9'
 
 exec_dirs = [
     'src/tld/bin/',
--- src/tld/__init__.py
+++ src/tld/__init__.py
@@ -2,12 +2,13 @@ from .utils import (
     get_fld,
     get_tld,
     get_tld_names,
+    parse_tld,
     Result,
     update_tld_names,
 )
 
 __title__ = 'tld'
-__version__ = '0.8'
+__version__ = '0.9'
 __author__ = 'Artur Barseghyan'
 __copyright__ = '2013-2018 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
@@ -15,6 +16,7 @@ __all__ = (
     'get_fld',
     'get_tld',
     'get_tld_names',
+    'parse_tld',
     'Result',
     'update_tld_names',
 )
