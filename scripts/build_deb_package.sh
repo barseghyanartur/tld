a81d3f67e6fc6097372e009ceca02e8d34cb8bf0
Artur Barseghyan <artur.barseghyan@gmail.com>
2018-07-09 22:06:33 +0200

    improved scripts

--- scripts/build_deb_package.sh
+++ scripts/build_deb_package.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
-python setup.py sdist bdist_wheel
-py2dsc-deb dist/tld-0.9.tar.gz
+
+version = ""
+
+if [[ $1 == "--version" ]]
+then
+    version="$2"
+    shift
+    shift
+    args="$@"
+else
+    echo "You should provide a --version argument."
+fi
+
+if [[ $version ]]
+then
+    python setup.py sdist bdist_wheel
+    py2dsc-deb "dist/tld-$version.tar.gz"
+fi
