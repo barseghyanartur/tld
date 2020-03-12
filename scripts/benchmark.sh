41e4c8a88b96481168553cc9e48500692b2089b5
Artur Barseghyan <artur.barseghyan@gmail.com>
2020-03-12 23:07:44 +0100

    More

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-pycallgraph graphviz -- benchmarks/profile.py
+pycallgraph \
+  --stdlib \
+  --include "tld.*" \
+  --include "urllib.*" \
+  --include "codecs.*" \
+  --include "functools.*" \
+  --include "os.*" \
+  --include "sys.*" \
+  --include "argparse.*" \
+  graphviz \
+  -- benchmarks/profile.py
