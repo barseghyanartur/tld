772677961d8a24703603acec9cab858482862858
Artur Barseghyan <artur.barseghyan@gmail.com>
2022-03-09 08:54:18 +0100

    Fixes in docs

--- scripts/rebuild_docs.sh
+++ scripts/rebuild_docs.sh
@@ -3,5 +3,5 @@
 ./scripts/install.sh
 rm docs/*.rst
 rm -rf builddocs/
-sphinx-apidoc src/tld --full -o docs -H 'tld' -A 'Artur Barseghyan <artur.barseghyan@gmail.com>' -f -d 20
+sphinx-apidoc src/tld src/tld/tests/* --full -o docs -H 'tld' -A 'Artur Barseghyan <artur.barseghyan@gmail.com>' -f -d 20
 cp docs/conf.py.distrib docs/conf.py
