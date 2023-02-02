a406e8cc0331339a8b10294bba0d51e8e3d5ea9b
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-02-02 22:44:30 +0100

    Add python 3.10 and 3.11 builds

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -5,3 +5,5 @@ python setup.py sdist bdist_wheel --python-tag py36
 python setup.py sdist bdist_wheel --python-tag py37
 python setup.py sdist bdist_wheel --python-tag py38
 python setup.py sdist bdist_wheel --python-tag py39
+python setup.py sdist bdist_wheel --python-tag py310
+python setup.py sdist bdist_wheel --python-tag py311
